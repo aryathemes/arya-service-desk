04b266f71a9a346ee62f2b915e57ce7054b87ce2
Luis A. Ochoa <alberto.0x7c3@gmail.com>
2019-09-10 12:49:16 -0500

    Adds the answers to the question list

--- arya-service-desk/src/Admin/FAQ.php
+++ arya-service-desk/src/Admin/FAQ.php
@@ -30,6 +30,10 @@ class FAQ
         /* Placeholders */
         add_filter( 'enter_title_here', [ $this, 'title' ], 10, 2 );
         add_filter( 'write_your_story', [ $this, 'body' ], 10, 2 );
+
+        /* List table */
+        add_filter( 'manage_edit-service-desk-faq_columns', [ $this, 'column' ],  5, 1 );
+        add_action( 'manage_service-desk-faq_posts_custom_column', [ $this, 'answer' ], 10, 2 );
     }
 
     /**
@@ -74,5 +78,33 @@ class FAQ
         }
 
         return $text;
+    }
+
+    /**
+     * Adds the answer column to the question list table.
+     *
+     * @since 1.0.0
+     */
+    public function column( $columns )
+    {
+        $date = $columns['date'];
+        unset( $columns['date'] );
+
+        $columns['answer'] = __( 'Answer', 'arya-service-desk' );
+        $columns['date'] = $date;
+
+        return $columns;
+    }
+
+    /**
+     * Displays the answer.
+     *
+     * @since 1.0.0
+     */
+    public function answer( $column, $post_id )
+    {
+        if ( 'answer' == $column ) {
+            echo wp_kses_post( get_post_field( 'post_content', $post_id ) );
+        }
     }
 }
