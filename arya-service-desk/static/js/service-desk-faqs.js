23519e5699bb94be0c752b593a452a02b1984008
Luis A. Ochoa <alberto.0x7c3@gmail.com>
2019-10-08 00:46:19 -0500

    Updates service-desk-faqs.js script

--- arya-service-desk/static/js/service-desk-faqs.js
+++ arya-service-desk/static/js/service-desk-faqs.js
@@ -1,7 +1,15 @@
 (function($) {
-    $( "#questions" ).accordion({
+
+    questions = $( ".questions" );
+
+    if ( ! questions.length ) {
+        return;
+    }
+
+    questions.accordion({
         collapsible: true,
         header: "h3",
         heightStyle: "content"
     });
+
 })( jQuery );
